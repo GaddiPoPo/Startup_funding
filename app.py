474fef3edd980ed985d6b7099dabe9290046e94f
Jagrat Sahni <97514947+GaddiPoPo@users.noreply.github.com>
2025-06-23 19:42:04 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-df=pd.read_csv("C:\\Users\\jagra\\Desktop\\Campusx\\startup-dashboard\\startup_cleaned.csv")
+df=pd.read_csv("startup_cleaned.csv")
 df['date'] = pd.to_datetime(df['date'],errors='coerce')
 df['month'] = df['date'].dt.month
 df['year'] = df['date'].dt.year
